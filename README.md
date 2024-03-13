892d927e309a66b0c3c988623a777aa70f726973
Raziel Rodrigues <razielx3@live.com>
2024-03-13 01:29:55 +0000

    feat: finalizando codigo

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # 30 days of code php
 
+Crieei esse repositorio com o intuito de estudar durante 30 dias, alguns conceitos de PHP que eu sempre achei interessante, a ideia aqui e não perder o foco e se aprofundar cada vez mais na minha linguagem preferida, com certeza se voce estiver lendo esse repositorio vai achar otimos insights (dentro de cada arquivo terá a parte teorica e o codigo)
 
-Yield
- - Fatal error: The "yield" expression can only be used inside a function in /app/public/day1/yield.php on line 8
+# Tech Stack
+
+O projeto será sempre feito com PHP puro e pra isso eu to usando o FrankenPHP pra subir o environment e eu me preocupar apenas com o código.
+
+# Getting started
+
+Você precisa do docker e ai e so rodar
+
+docker compose up
+
+e depois acessar localhost
+
+# Content List
+
+Aqui vou listar os assuntos que estudei ou que quero estudar em ordem cronologica (eu nao posso perder um dia)
+
+12/03/2024 - Generators (dia 1) = done
+13/03/2024 - Fibers (dia 2)
+14/03/2024 - Attributes (dia 3)
+15/03/2024 - References (dia 4)
+
+# TODO
+    - Criar tradução para inglês
+    - Decorar readme
--- day1/yield.php
+++ day1/yield.php
@@ -1,10 +1,21 @@
 <?php
 
+# https: //www.php.net/manual/en/language.generators.syntax.php
 
-echo 'treinando yield <hr>';
+/**
+ * Então vendo que o for não ia ser a melhor solução sempre
+ * os criadores do PHP decidiram implementar os Generator,
+ * generators sao basicamente funções que retornam o resultado
+ * de algo iteravel sem precisar salvar o resultado da iteração
+ * em uma variavel, uma vez que o generator logo após retornar
+ * iterar já retorna o resultado ou seja não sendo mais
+ * necessario fazer o famoso $array[] = $value;
+ */
 
 
+echo '<h1> Generators: </h1> <hr>';
 
+# USO BÁSICO
 function yieldCall()
 {
     for ($i = 0; $i < 10; $i++) {
@@ -12,17 +23,14 @@ function yieldCall()
     }
 }
 
-
 $generator = yieldCall();
-
 var_dump($generator);
-echo 'treinando yield <hr>';
 
 foreach ($generator as $value) {
-    var_dump($value);
+    echo $value;
 }
 
-echo 'treinando yield 2 <hr>';
+echo '<hr>';
 
 $csv = 'nome;raziel';
 
@@ -36,7 +44,6 @@ function parserYield($input)
 foreach (parserYield($csv) as $key => $value) {
     echo ($key);
     echo '<hr>';
-
     echo $value;
 }
 
@@ -54,3 +61,36 @@ function &genYield()
 foreach (genYield() as &$key) {
     echo (--$key);
 }
+
+echo '<hr>';
+
+class Test
+{
+}
+
+function y()
+{
+    yield 3;
+    yield 4;
+}
+
+function x()
+{
+    yield 1;
+    yield from y();
+}
+
+
+$x = x();
+
+foreach ($x as $value) {
+    var_dump($value);
+}
+
+$x->getReturn();
+
+var_dump(is_array($x->getReturn()));
+
+echo '<hr>';
+
+echo '<h1> Exemplo benchmark: </h1> <hr>';
